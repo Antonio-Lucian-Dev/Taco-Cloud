9ec4cfa2e1979025d49dc316e13cf9d784ed8b31
Antonio Lucian <web.antonio.popa@gmail.com>
2021-09-03 17:04:18 +0300

    Added order object with validator

--- src/main/java/com/asusoftware/tacocloud/model/Order.java
+++ src/main/java/com/asusoftware/tacocloud/model/Order.java
@@ -1,16 +1,28 @@
 package com.asusoftware.tacocloud.model;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 
 @Data
 public class Order {
 
+    @NotBlank(message = "Name is required")
     private String name;
+    @NotBlank(message = "Street is required")
     private String street;
+    @NotBlank(message = "City is required")
     private String city;
+    @NotBlank(message = "State is required")
     private String state;
+    @NotBlank(message = "Zip code is required")
     private String zip;
+    @CreditCardNumber(message = "Not valid credit")
     private String ccNumber;
+    @Pattern(regexp = "^(0[1-9]| 1 [0-2]) ([)")
     private String ccExpiration;
     private String ccCVV;
 }
--- src/main/java/com/asusoftware/tacocloud/model/Taco.java
+++ src/main/java/com/asusoftware/tacocloud/model/Taco.java
@@ -2,11 +2,17 @@ package com.asusoftware.tacocloud.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class Taco {
 
+    @NotNull
+    // Il Size permette di specificare la dimensione minima o massima ed il messaggio da mostrare in caso non si rispetti questa condizione
+    @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<String> ingredients;
 }
