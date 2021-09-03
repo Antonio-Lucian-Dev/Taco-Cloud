f67f3ad95a37bc7808e4ddef2febc3cfe739fa2f
Antonio Lucian <web.antonio.popa@gmail.com>
2021-09-03 11:36:07 +0300

    Added functionality of design taco and order

--- src/main/java/com/asusoftware/tacocloud/controller/DesignTacoController.java
+++ src/main/java/com/asusoftware/tacocloud/controller/DesignTacoController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -15,7 +16,7 @@ import java.util.stream.Collectors;
 
 import com.asusoftware.tacocloud.model.Ingredient.Type;
 
-@Slf4j
+@Slf4j // Permette di avere un log sulla situazione
 @Controller
 @RequestMapping("/design")
 public class DesignTacoController {
@@ -47,5 +48,11 @@ public class DesignTacoController {
                 .stream()
                 .filter(x -> x.getType().equals(type))
                 .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public String processDesign(Taco design) {
+        log.info("Processing design: " + design);
+        return "redirect:/orders/current";
     }
 }
