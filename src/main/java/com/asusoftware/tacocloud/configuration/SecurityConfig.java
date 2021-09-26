ed8bca310a68948ece15e0dcd8b48ee1159bb753
Antonio Lucian <web.antonio.popa@gmail.com>
2021-09-26 12:41:49 +0300

    Added Securing Requests

--- src/main/java/com/asusoftware/tacocloud/configuration/SecurityConfig.java
+++ src/main/java/com/asusoftware/tacocloud/configuration/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -56,5 +57,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth
                 .userDetailsService(userDetailsService)
                 .passwordEncoder(encoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       /* Prima variante
+       http
+                .authorizeRequests() // Ci permette di specificare i path i pattern e le security requirements per quei path
+                .antMatchers("/design", "/orders") // Specifica i path per i quale si vuole aggiungere una specifica security
+                // Permette l'accesso se l'utente ha questo ruolo
+                .hasRole("ROLE_USER") // Devono essere per gli utenti che hanno il granted role su ROLE_USER
+                .antMatchers("/", "/**") // Questi path
+                .permitAll(); // sono permessi per tutti gli utenti */
+
+        /* Seconda varinate con SpEl expression, ti permette di definire piu espressioni per i path specifici */
+        http
+                .authorizeRequests()
+                .antMatchers("/design", "/orders")
+                .access("hasRole('ROLE_USER')")
+                .antMatchers("/", "/**")
+                .access("permitAll");
+
+        /* Se vogliamo permettere agli utenti con il role di user di creare i taco on Tuesday
+
+        http
+                .authorizeRequests()
+                .antMatchers("/design", "/orders")
+                .access("hasRole('ROLE_USER') && " +
+                        "T(java.util.Calendar).getInstance().get(" +
+                        "T(java.util.Calendar).DAY_OF_WEEK) == " +
+                        "T(java.util.Calendar).TUESDAY")
+                .antMatchers("/", "/**")
+                .access("permitAll"); */
     }
 }
